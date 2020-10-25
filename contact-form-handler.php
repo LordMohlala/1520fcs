f2d642f8c8ca53a949edd5595a343b468360fc3d
LordMohlala <71822586+LordMohlala@users.noreply.github.com>
2020-10-25 17:45:19 +0200

    first commit

---
+++ contact-form-handler.php
@@ -0,0 +1,24 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'kamogelomohlala66@gmail.com';
+
+    $email_subject = 'New Form Submission';
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                      "User Message: $message.\n";
+    
+                      
+    $to = "kamogelomohlala66@outlook.com";
+    
+    $headers = "From: $email_from \r\n";
+
+    $headers = "Reply to: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+?>
